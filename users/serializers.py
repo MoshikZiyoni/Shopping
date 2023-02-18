4d1d85107f8459c5465079a33d9466adf15d8362
Moshik-laptop <moshiktm1994@gmail.com>
2023-02-18 23:48:11 +0200

    Register

--- users/serializers.py
+++ users/serializers.py
@@ -51,3 +51,21 @@ class UserSerializer(serializers.ModelSerializer):
             'first_name',
             'last_name',
         ]
+
+
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            validated_data['username'],
+            validated_data['email'],
+            validated_data['password']
+        )
+        return user
--- users/views.py
+++ users/views.py
@@ -5,24 +5,26 @@ from rest_framework import views, status
 from rest_framework.response import Response
 from rest_framework import generics
 from . import serializers
-
+from django.contrib.auth.models import User
 # Login
 class LoginView(views.APIView):
     # This view should be accessible also for unauthenticated users.
     permission_classes = (permissions.AllowAny,)
-
+    
     def post(self, request, format=None):
-        serializer = serializers.LoginSerializer(data=self.request.data,
-                                                 context={'request': self.request})
-        serializer.is_valid(raise_exception=True)
-        user = serializer.validated_data['user']
-        login(request, user)
-        return Response(None, status=status.HTTP_202_ACCEPTED)
+        try:
+            serializer = serializers.LoginSerializer(data=self.request.data,context={'request': self.request})
+            serializer.is_valid(raise_exception=True)
+            user = serializer.validated_data['user']
+            login(request, user)
+            return Response(None, status=status.HTTP_202_ACCEPTED)
+        except Exception as e:
+            print(e)
+            return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
 # Logout function
 class LogoutView(views.APIView):
     permission_classes = (permissions.AllowAny,)
-
     def get(self, request, format=None):
         logout(request)
         response = Response(None, status=status.HTTP_204_NO_CONTENT)
@@ -33,16 +35,27 @@ class LogoutView(views.APIView):
 ####
 class ProfileView(generics.RetrieveAPIView):
     serializer_class = serializers.UserSerializer
-
     def get_object(self):
         return self.request.user
 
 # Register
+# class RegisterView(views.APIView):
+#     permission_classes = (permissions.AllowAny,)
+#     def post(self, request, format=None):
+#         serializer = serializers.UserSerializer(data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         user.set_password(request.data['password']) # Add this line to save the password
+#         user = serializer.save()
+#         login(request, user)
+#         return Response(None, status=status.HTTP_201_CREATED)
 class RegisterView(views.APIView):
-    permission_classes = (permissions.AllowAny,)
-    def post(self, request, format=None):
-        serializer = serializers.UserSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        user = serializer.save()
-        login(request, user)
-        return Response(None, status=status.HTTP_201_CREATED)
+    def post(self, request):
+        serializer = serializers.RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            user = User.objects.create_user(
+                serializer.validated_data['username'],
+                serializer.validated_data['email'],
+                serializer.validated_data['password']
+            )
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
