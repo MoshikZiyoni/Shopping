496442e9d7e41c1481e0d2b4fbe62c5984c4557f
Moshik-laptop <moshiktm1994@gmail.com>
2023-02-23 15:13:03 +0200

    sending email after checkout

--- Shopping/settings.py
+++ Shopping/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = ('django-insecure-t)p&a1fznsq=l@d+bq+gul8(fs@v6_am=8t!%ug7-$s_v4#am3')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', True)
